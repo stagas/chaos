699538dcbe6a1f14080cc0a2128ec5727fabc771
George Stagas <gstagas@gmail.com>
2010-10-16 23:44:18 +0300

    implement queue, improved benchmarks

--- bench.js
+++ bench.js
@@ -1,45 +1,62 @@
 //
 
 var db = require('./chaos')('test')
+  , assert = require('assert')
 
+var best = {writes: 0, reads: 0}
+  , avg = {writes: 0, writesCnt: 0, reads: 0, readsCnt: 0}
+
+db.maxOpenFiles = 30
+  
 var big = []
 for (var i=1000; i--; ) {
   big.push(i)
 }
 
-var obj = JSON.stringify({
-  there: 'is'
-, only: 'chaos'
-, butterfly: ['says', ['there', 'is', 'only', 'chaos']]
-, pi: Math.PI
-, big: big
-})
+var objects = [
+  'tiny'
+  
+, 'hello I am a medium sized string'
 
-var obj = 'hello' // comment this out to use the big object above
+, JSON.stringify({
+    there: 'is'
+  , only: 'chaos'
+  , butterfly: ['says', ['there', 'is', 'only', 'chaos']]
+  , pi: Math.PI  
+  })
+  
+, JSON.stringify({
+    there: 'is'
+  , only: 'chaos'
+  , butterfly: ['says', ['there', 'is', 'only', 'chaos']]
+  , pi: Math.PI
+  , big: big
+  })  
+]
 
-function bench(what, num, conc, cb) {
-  console.log('    clients:', conc)
+function bench(obj, what, num, cb) {
+  console.log(' obj length:', obj.length)
   console.log(' operations:', num)
   console.log('-------------------')
   
   switch (what) {
     case 'all':
-      sets(num, conc, function() {
-        gets(num, conc, function() {
+      sets(obj, num, function() {
+        gets(obj, num, function() {
           console.log('')
           cb()
         })
       })
       break
     case 'sets':
-      sets(num, conc, function() {
+      sets(obj, num, function() {
         console.log('')
         cb()
       })
       break
     case 'gets':
     default:
-      gets(num, conc, function() {
+      gets(obj, num, function() {
         console.log('')
         cb()
       })
@@ -47,129 +64,115 @@ function bench(what, num, conc, cb) {
   }
 }
 
-function sets(num, conc, cb) {
-  var timer = new Date()
-    , done = 1
+function sets(obj, num, cb) {
+  var done = 0
     , clients = 0
+    , timer = new Date()
 
-  var next = function(i) {
-    if (done < num) {
-      if (clients < conc) {
-        clients++
-        
-        db.set(i, obj, function(err) {
-          done++
-          clients--
-        })
-        
-        i++
-        next(i)
-      } else {
-        process.nextTick(function() { next(i) })
+  for (var i=num; i--; ) {
+    db.set(i, obj, function(err) {
+      done++
+      if (done === num) {
+        var result = ( (num) / ((new Date() - timer) / 1000))
+        if (result > best.writes) best.writes = result
+        avg.writes += result
+        avg.writesCnt += 1
+        console.log('writes:', result.toFixed(2) + '/s')
+        cb()
       }
-    } else {
-      console.log('writes:', ( (num) / ((new Date() - timer) / 1000)).toFixed(2) + '/s')
-      return cb()
-    }
+    })
   }
-  
-  next(0)
 }
 
-function gets(num, conc, cb) {
-  var timer = new Date()
-    , done = 1
+function gets(obj, num, cb) {
+  var done = 0
     , clients = 0
+    , timer = new Date()
 
-  var next = function(i) {
-    if (done < num) {
-      if (clients < conc) {
-        clients++
-        
-        db.get(i, function(err, data) {
-          done++
-          clients--
-        })
-        
-        i++
-        next(i)
-      } else {
-        process.nextTick(function() { next(i) })
+  for (var i=num; i--; ) {
+    db.get(i, function(err, data) {
+      done++
+      if (done === num) {
+        var result = ( (num) / ((new Date() - timer) / 1000))
+        if (result > best.reads) best.reads = result
+        avg.reads += result
+        avg.readsCnt += 1
+        console.log('reads:', result.toFixed(2) + '/s')
+        cb()
       }
-    } else {
-      console.log('reads:', ( (num) / ((new Date() - timer) / 1000)).toFixed(2) + '/s')
-      return cb()
-    }
+    })
   }
-  
-  next(0)
 }
 
 var scenario = [
 
-  ['sets', 1000, 10]
-, ['sets', 5000, 10]
-, ['sets', 10000, 10]
-  
-, ['sets', 1000, 100]
-, ['sets', 5000, 100]
-, ['sets', 10000, 100]
+  ['sets', 100]
+, ['sets', 500]
+, ['sets', 1000]
+, ['sets', 2000]
+//, ['sets', 5000]
+//, ['sets', 10000]
 
-, ['sets', 1000, 300]
-, ['sets', 5000, 300]
-, ['sets', 10000, 300]
-
-, ['sets', 1000, 600]
-, ['sets', 5000, 600]
-, ['sets', 10000, 600]
-
-, ['sets', 1000, 1000]
-, ['sets', 5000, 1000]
-, ['sets', 10000, 1000]
-
-, ['sets', 1000, 1500]
-, ['sets', 5000, 1500]
-, ['sets', 10000, 1500]
-
-, ['gets', 1000, 10]
-, ['gets', 5000, 10]
-, ['gets', 10000, 10]
-  
-, ['gets', 1000, 100]
-, ['gets', 5000, 100]
-, ['gets', 10000, 100]
-
-, ['gets', 1000, 300]
-, ['gets', 5000, 300]
-, ['gets', 10000, 300]
-
-, ['gets', 1000, 600]
-, ['gets', 5000, 600]
-, ['gets', 10000, 600]
-
-, ['gets', 1000, 1000]
-, ['gets', 5000, 1000]
-, ['gets', 10000, 1000]
-
-, ['gets', 1000, 1500]
-, ['gets', 5000, 1500]
-, ['gets', 10000, 1500]
+, ['gets', 100]
+, ['gets', 500]
+, ['gets', 1000]
+, ['gets', 2000]
+//, ['gets', 5000]
+//, ['gets', 10000]
 
 ]
 
-scenarioLen = scenario.length
+var scenarioLen = scenario.length
 
-var next = function(i) {
+var next = function(i, o) {
   if (i < scenarioLen) {
-    
-    bench(scenario[i][0], scenario[i][1], scenario[i][2], function() {
+    bench(objects[o], scenario[i][0], scenario[i][1], function() {
       setTimeout(function() {
-        next(++i)
-      }, scenario[i][1] / 2) // give some time for the hd to breath
+        next(++i, o)
+      }, scenario[i][1] / 3) // give some time for the hd to breath
     })
-    
   } else {
-    console.log('all done')
+    o++
+    if (o===objects.length) {
+      console.log('---------------------')
+      console.log('version:', db.version)
+      console.log('max open files:', db.maxOpenFiles)
+      console.log('')
+      console.log('best writes:', best.writes.toFixed(2) + '/s')
+      console.log('best reads:', best.reads.toFixed(2) + '/s')
+      console.log('avg writes:', (avg.writes / avg.writesCnt).toFixed(2) + '/s')
+      console.log('avg reads:', (avg.reads / avg.readsCnt).toFixed(2) + '/s')
+      console.log('---------------------')
+      console.log('')
+      console.log('all done!')
+    } else {
+      next(0, o)
+    }
+  }
+}
+
+var consistency = function(cb) {
+  var done = 0
+    , num = 100
+  
+  console.log('writes...')
+  for (var i=num; i--; ) {
+    db.set(i, '1234567890', function(err) {
+      done++
+      if (done===num) {
+        done = 0
+        console.log('reads...')
+        for (var i=num; i--; ) {
+          db.get(i, function(err, data) {
+            done++
+            assert.equal(data, '1234567890', 'Consistency error!')
+            if (done===num) {
+              cb()
+            }
+          })
+        }
+      }
+    })
   }
 }
 
@@ -177,9 +180,14 @@ var start = function() {
   if (!db.ready) {
     setTimeout(function() { start() }, 1000)
   } else {
-    console.log('benchmark starting...')
-    console.log('')
-    next(0)
+    console.log('checking consistency...')
+    consistency(function() {
+      console.log('done.')
+      console.log('=====================')
+      console.log('benchmark starting...')
+      console.log('')
+      next(0, 0)
+    })
   }
 }
 
--- chaos.js
+++ chaos.js
@@ -1,5 +1,5 @@
 /*
- * chaos v0.0.1
+ * chaos v0.0.2
  *
  * by stagas
  *
@@ -9,10 +9,44 @@ var sys = require('sys')
   , fs = require('fs')
   , path = require('path')  
   , crypto = require('crypto')
+  , EventEmitter = require('events').EventEmitter
 
+// creationix's fast Queue
+var Queue = function() {
+  this.tail = [];
+  this.head = Array.prototype.slice.call(arguments);
+  this.offset = 0;
+  // Lock the object down
+  Object.seal(this);
+}
+
+Queue.prototype = {
+  shift: function shift() {
+    if (this.offset === this.head.length) {
+      var tmp = this.head;
+      tmp.length = 0;
+      this.head = this.tail;
+      this.tail = tmp;
+      this.offset = 0;
+      if (this.head.length === 0) return;
+    }
+    return this.head[this.offset++];
+  },
+  push: function push(item) {
+    return this.tail.push(item);
+  },
+  get length() {
+    return this.head.length - this.offset + this.tail.length;
+  }
+}
+  
 var Chaos = exports.Chaos = function(dbName) {
   if (!(this instanceof Chaos)) return new Chaos(dbName)
   var self = this
+  
+  this.version = 'v0.0.2'
+  
+  EventEmitter.call(this)
   
   this.dbName = dbName
 
@@ -31,10 +65,23 @@ var Chaos = exports.Chaos = function(dbName) {
   this._hashAlgo = 'sha1'
   this._hashEnc = 'hex'
   
-  this._maxOpenFiles = 30
+  this.maxOpenFiles = 30
   this._openFiles = 0
+
+  this._writeQueue = new Queue()
+  this._readQueue = new Queue()
+
+  this._queued = false
+  
+  this.on('queue', function() {
+    if (!self._queued) {
+      self._queued = true
+      self._drain()
+    }
+  })
 }
 
+sys.inherits(Chaos, EventEmitter)
 Chaos.Chaos = Chaos
 module.exports = Chaos
 
@@ -67,30 +114,52 @@ Chaos.prototype._hash = function(key) {
   return {a: hash.substr(0,2), b: hash.substr(2,2), c: hash.substr(4)}
 }
 
-Chaos.prototype.set = function(key, val, cb) {
-  if (this._openFiles < this._maxOpenFiles) {
-    this._openFiles++
-    this._write(this._hash(key), val, cb)
-  } else {
-    var self = this
-    process.nextTick(function() { self.set(key, val, cb) })
+Chaos.prototype._queue = function(what, which, then) {
+  this['_' + what + 'Queue'].push([which, then])
+  
+  this.emit('queue')
+}
+
+Chaos.prototype._drain = function() {
+  var oper
+  
+  if (this._openFiles < this.maxOpenFiles) {
+    if (this._writeQueue.length) {
+      oper = this._writeQueue.shift()
+      this._write(oper[0][0], oper[0][1], oper[1])
+    }
+    
+    if (this._readQueue.length) {
+      oper = this._readQueue.shift()
+      this._read(oper[0], oper[1])
+    }
   }
+
+  if (this._writeQueue.length || this._readQueue.length) {
+    var self = this
+    
+    process.nextTick(function() {
+      self._drain()
+    })
+  } else {
+    this._queued = false
+  }
+}
+
+Chaos.prototype.set = function(key, val, cb) {
+  this._queue('write', [key, val], cb)
 }
 
 Chaos.prototype.get = function(key, cb) {
-  if (this._openFiles < this._maxOpenFiles) {
-    this._openFiles++
-    this._read(this._hash(key), cb)
-  } else {
-    var self = this
-    process.nextTick(function() { self.get(key, cb) })
-  }
+  this._queue('read', key, cb)
 }
 
-
-Chaos.prototype._write = function(pos, val, cb) {    
+Chaos.prototype._write = function(key, val, cb) {    
   var self = this
+    , pos = this._hash(key)
 
+  this._openFiles++
+  
   fs.writeFile(self.dbName +'/'+ pos.a +'/'+ pos.b +'/'+ pos.c, val, 'utf8', function(err) {
     self._openFiles--
     
@@ -98,9 +167,12 @@ Chaos.prototype._write = function(pos, val, cb) {
   })
 }
 
-Chaos.prototype._read = function(pos, cb) {
+Chaos.prototype._read = function(key, cb) {
   var self = this
-
+    , pos = this._hash(key)
+    
+  this._openFiles++
+  
   fs.readFile(this.dbName +'/'+ pos.a +'/'+ pos.b +'/'+ pos.c, function(err, data) {
     self._openFiles--
     
